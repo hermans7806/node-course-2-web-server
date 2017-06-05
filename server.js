1996317231f326484bc93913595ba726e4700443
Herman Sutanto <herman.sutanto@reebonz.com>
2017-06-05 12:32:13 +0800

    adding project page

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+    welcomeMessage: 'My Portfolio'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
